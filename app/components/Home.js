9e2f5819c8261599bc2110c7f2f1d5eb33b766ba
Matt Warner <mwarner@amazon.com>
2022-04-10 00:10:26 -0700

    Add friendly id for debugging + cleanup dead code

--- app/components/Home.js
+++ app/components/Home.js
@@ -14,21 +14,17 @@ export default class Home extends React.Component {
     ComponentRelayManager.get().initiateBridge();
   }
 
+  handleCreateDailyNote() {
+    console.log('do some creatin..');
+  }
+
   render() {
     return (
       <div>
         <p>Component is ready.</p>
-
-        {this.state.note &&
-          <div>
-            <p>
-              Working note title: <strong>{this.state.note.content.title}</strong>
-            </p>
-            <p>
-              Working note content: <strong>{this.state.note.content.text}</strong>
-            </p>
-          </div>
-        }
+        <button onClick={this.handleCreateDailyNote}>
+          Create daily note
+        </button>
       </div>
     )
   }
